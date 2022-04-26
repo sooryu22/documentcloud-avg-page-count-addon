c472d0c5c33381bec8b1cd42e067c2d4a1840eb3
Sooryu Kim <sooryu22@gmail.com>
2022-04-26 15:54:46 -0400

    updating title in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """
-This is a Average Page Count add-on for DocumentCloud.
+This is a Page Stats add-on for DocumentCloud.
+This will return a csv file containing total pages, average number of pages, 
+and link the largest and small document with their number of pages for a given selection of documents.
 
 It demonstrates how to write a add-on which can be activated from the
 DocumentCloud add-on system and run using Github Actions.  It receives data
@@ -12,7 +14,7 @@ import csv
 
 
 class AvgPageCount(AddOn):
-    """An Average Page Count Add-On for DocumentCloud."""
+    """A Page Stats Add-On for DocumentCloud."""
 
     def main(self):
         """The main add-on functionality goes here."""
@@ -22,7 +24,7 @@ class AvgPageCount(AddOn):
             self.set_message("Please select at least one document")
             return
 
-        self.set_message("Beginning average page count!")
+        self.set_message("Beginning page stats!")
 
         doc_selected = len(self.documents)
         page_total = 0
@@ -42,7 +44,7 @@ class AvgPageCount(AddOn):
 
         avg_page_cnt = round(page_total/doc_selected, 2)
 
-        with open("avg_page_count_for_"+str(doc_selected)+"_docs"+".csv", "w+") as file_:
+        with open("page_stats_for_"+str(doc_selected)+"_docs"+".csv", "w+") as file_:
             field_names = ['total_page',
                            'average_page_count', 'min_page_count', 'max_page_count', 'min_page_url', 'max_page_url']
             writer = csv.DictWriter(file_, fieldnames=field_names)
@@ -55,7 +57,7 @@ class AvgPageCount(AddOn):
                              'min_page_url': min_page_doc.pdf_url,
                              'max_page_url': max_page_doc.pdf_url})
             self.upload_file(file_)
-        self.set_message("Average page count end!")
+        self.set_message("Page stats end!")
 
 
 if __name__ == "__main__":
